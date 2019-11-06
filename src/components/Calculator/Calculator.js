d53b8fc23fa3a574ebc9491ce7e084de81381ebb
Wade Willequer <Wade.Willequer@gmail.com>
2019-11-06 14:50:39 -0600

    Added the Screen and ResultsScreen components to the calculator body

--- src/components/Calculator/Calculator.js
+++ src/components/Calculator/Calculator.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 import './Calculator.css';
-
+import Screen from '../Screen/Screen';
 
 // Main calculator component. Contains our other calculator components
 class Calculator extends Component {
     render() {
         return (
             <div className="Calculator">
-
+                <Screen />
             </div>
         );
     }
---
+++ src/components/Screen/Screen.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './Screen.css'
+import ResultsScreen from '../ResultsScreen/ResultsScreen';
+
+// Component that holds the results screen and calculations
+class Screen extends Component {
+    render() {
+        return (
+            <div className='Screen'>
+                <ResultsScreen>0</ResultsScreen>
+            </div>
+        );
+    }
+}
+
+export default Screen;
